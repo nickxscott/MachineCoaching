ffce65f1b0137f4f4003011475184962eb2709fe
Nicholas Scott <nickxscott@gmail.com>
2024-01-07 17:48:06 -0500

    login/registration progress

--- blueprints/forms.py
+++ blueprints/forms.py
@@ -1,7 +1,21 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, SelectField, RadioField, SelectMultipleField, BooleanField
-from wtforms.validators import InputRequired, Email
+from wtforms.validators import InputRequired, Email, Length, EqualTo
 
 class loginForm(FlaskForm):
 	email = StringField("email", validators=[InputRequired()],render_kw={"placeholder": "Email Address"}) 
-	pwd=PasswordField("pwd", validators=[InputRequired()],render_kw={"placeholder": "Password"}) 
+	pwd=PasswordField("pwd", validators=[InputRequired(), Length(min=8, message='Too short')],render_kw={"placeholder": "Password"}) 
+
+class regForm(FlaskForm):
+	f_name = StringField("first", validators=[InputRequired()])
+	l_name = StringField("last", validators=[InputRequired()])
+	email = StringField("email", validators=[	InputRequired(), 
+												Email(	check_deliverability=True, 
+														message='Not a valid email address')],
+								render_kw={"placeholder": "Email Address"}) 
+	pwd=PasswordField(	"pwd", 
+						validators=[InputRequired(), 
+									Length(	min=8, 
+											message='Too short')], 
+						render_kw={"placeholder": "Password"}) 
+	confirm_pwd = PasswordField('Confirm Password', validators=[InputRequired(), EqualTo('pwd', message='Both password fields must be equal!')])
--- blueprints/login/login.py
+++ blueprints/login/login.py
@@ -1,14 +1,46 @@
-from flask import Blueprint, render_template
+
+#imports
+from flask import Blueprint, render_template, request, flash
+from flask_login import login_user, login_required, logout_user, current_user
+
+#custom imports
 from ..forms import *
+from app import bcrypt, login_manager
 
 
 login_bp = Blueprint('login', __name__, template_folder='../templates')
 
-@login_bp.route('/')
-def login():
-	form = loginForm()
-	return render_template('/login/login.html', form=form)
 
-@login_bp.route('/test')
-def test():
-	return render_template('/login/test.html')
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
+
+
+@login_bp.route('/', methods=['GET','POST'])
+def login():
+
+	form = loginForm()
+	user = form.email.data
+	pw = form.pwd.data
+
+	if request.method=='POST' and form.validate_on_submit():
+		hashed_password = bcrypt.generate_password_hash(pw).decode('utf-8')
+		flash(user)
+		flash(pw)
+		flash(hashed_password)
+		if bcrypt.check_password_hash('$2b$12$bM5kzsMl5CsL0Yr4eB.Dm.jrXKk59ByG0vwAOtgSAa9yk7hIggPky', pw):
+			flash('hashing worked')
+
+		else:
+			flash('it didnt work')
+		return render_template('/login/login.html', form=form)
+
+	else:
+		for fieldName, errorMessages in form.errors.items():
+			for err in errorMessages:
+				flash(err )
+		return render_template('/login/login.html', form=form)
+
+@login_bp.route('/register')
+def reg():
+	return render_template('/login/register.html')
